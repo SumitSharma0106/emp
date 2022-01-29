9003ab274a5924825c8aa8a6b77e3d9753006c87
Sumit Sharma <sumit.sharma@ihg.com>
2022-01-29 23:11:46 +0530

    Add fiegn dependency

--- user-service/src/main/java/com/app/user/UserServiceApplication.java
+++ user-service/src/main/java/com/app/user/UserServiceApplication.java
@@ -1,15 +1,18 @@
 package com.app.user;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
@@ -20,6 +23,11 @@ public class UserServiceApplication {
 	@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
+	}
+
+	@Bean
+	Logger.Level feignLoggerLevel(){
+		return Logger.Level.FULL;
 	}
 
 }
--- user-service/src/main/java/com/app/user/service/UserService.java
+++ user-service/src/main/java/com/app/user/service/UserService.java
@@ -2,9 +2,12 @@ package com.app.user.service;
 
 import com.app.user.VO.Department;
 import com.app.user.VO.ResponseVO;
+import com.app.user.config.DepartmentServiceClient;
 import com.app.user.entity.User;
 import com.app.user.repository.UserRepository;
+import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -19,6 +22,9 @@ public class UserService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DepartmentServiceClient departmentServiceClient;
+
     public User saveUser(User user) {
         log.info("Save User : " + user.getUserId());
         return userRepository.save(user);
@@ -27,11 +33,17 @@ public class UserService {
     public ResponseVO getUserByDepartmentId(Long userId) {
         ResponseVO responseVO=new ResponseVO();
         User user=userRepository.findByUserId(userId);
-        Department department=
-        restTemplate.
-                getForObject("http://DEPARTMENT-SERVICE/services/department/getDepartment/" + user.getDepartmentId(),Department.class);
+//        Department department=
+//        restTemplate.
+//                getForObject("http://DEPARTMENT-SERVICE/services/department/getDepartment/" + user.getDepartmentId(),Department.class);
+        Department departmentDetail = null;
+        try {
+            departmentDetail=departmentServiceClient.getDepartmentID(user.getDepartmentId());
+        } catch (FeignException e) {
+            log.info(e.getLocalizedMessage());
+        }
         responseVO.setUser(user);
-        responseVO.setDepartment(department);
+        responseVO.setDepartment(departmentDetail);
         return responseVO;
     }
 }
