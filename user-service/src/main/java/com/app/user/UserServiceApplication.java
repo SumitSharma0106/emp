97a7ee2e63ba9019359fd2695ca8e64f111a8b70
SumitSharma0106 <sumit4652@gmail.com>
2022-03-01 15:28:55 +0530

    Pushing changes

--- user-service/src/main/java/com/app/user/UserServiceApplication.java
+++ user-service/src/main/java/com/app/user/UserServiceApplication.java
@@ -3,6 +3,7 @@ package com.app.user;
 import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,13 +11,21 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
 		SpringApplication.run(UserServiceApplication.class, args);
+		InetAddress inetAddress = InetAddress.getLocalHost();
+
+		System.out.println("My hostname is : " + inetAddress.getHostName());
+		System.out.println("My IP address is  : " + inetAddress);
 	}
 
 	@Bean
@@ -29,5 +38,6 @@ public class UserServiceApplication {
 	Logger.Level feignLoggerLevel(){
 		return Logger.Level.FULL;
 	}
+
 
 }
